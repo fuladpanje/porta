aa213df967a44c587ae39283de2158887307756c
fuladpanje <takbaran.com@gmail.com>
2026-07-29 15:12:31 +0330

    Fix web.php: serve React SPA for all non-API routes

--- backend/routes/web.php
+++ backend/routes/web.php
@@ -2,6 +2,6 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return response()->json(['message' => 'Porta API']);
-});
+Route::get('/{any}', function () {
+    return file_get_contents(public_path('index.html'));
+})->where('any', '.*');
